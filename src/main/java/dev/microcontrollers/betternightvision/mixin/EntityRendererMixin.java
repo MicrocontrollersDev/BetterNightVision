77a35ff95809d3d3f26b1ccd7ed0cf78cb907a67
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-07-30 02:02:11 -0700

    1.12.2 support

--- src/main/java/dev/microcontrollers/betternightvision/mixin/EntityRendererMixin.java
+++ src/main/java/dev/microcontrollers/betternightvision/mixin/EntityRendererMixin.java
@@ -3,6 +3,9 @@ package dev.microcontrollers.betternightvision.mixin;
 import dev.microcontrollers.betternightvision.config.Config;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.entity.EntityLivingBase;
+//#if MC==1.12.2
+//$$ import net.minecraft.init.MobEffects;
+//#endif
 import net.minecraft.potion.Potion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +16,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class EntityRendererMixin {
     @Inject(method = "getNightVisionBrightness", at = @At("HEAD"), cancellable = true)
     private void cleanerNightVision(EntityLivingBase entitylivingbaseIn, float partialTicks, CallbackInfoReturnable<Float> cir) {
+        //#if MC==1.8.9
         if (Config.cleanerNightVision) cir.setReturnValue(!(entitylivingbaseIn.getActivePotionEffect(Potion.nightVision).getDuration() < 200) ? 1.0F : (float) entitylivingbaseIn.getActivePotionEffect(Potion.nightVision).getDuration() / 200F);
+        //#else
+        //$$ if (Config.cleanerNightVision) cir.setReturnValue(!(entitylivingbaseIn.getActivePotionEffect(MobEffects.NIGHT_VISION).getDuration() < 200) ? 1.0F : (float) entitylivingbaseIn.getActivePotionEffect(MobEffects.NIGHT_VISION).getDuration() / 200F);
+        //#endif
         if (Config.disableNightVision) cir.setReturnValue(0F);
     }
 }
